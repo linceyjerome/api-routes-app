5964df17af3bca2d664642d53a2580430a4b8269
lincey jérôme <lj_jerome@hotmail.com>
2022-11-08 00:23:41 -0500

    Adding DevExtreme for the UI Components

--- components/kindergardenList.tsx
+++ components/kindergardenList.tsx
@@ -1,4 +1,19 @@
 //TODO : implémenter datagrid pour les données
+import 'devextreme/dist/css/dx.light.css';
+
+import {
+    Column,
+    ColumnChooser,
+    DataGrid,
+    FilterPanel,
+    FilterRow,
+    GroupPanel,
+    HeaderFilter,
+    Pager,
+    Scrolling,
+    SearchPanel,
+    StateStoring
+} from 'devextreme-react/data-grid';
 
 import { Info } from '../interfaces'
 import useSWR from 'swr'
@@ -12,104 +27,36 @@ type InfoProps = {
 
 
 export default function KinderGardenListComponent({ info }: InfoProps) {
-    const [pageIndex, setPageIndex] = useState(0);
-    const { data } = useSWR(`/api/info`, fetcher)
-    const [searchWord, setSearchWord] = useState<string>("");
 
-    const globalSearch = (): Info[] => {
-
-        const list = data.filter((value: Info) => {
-            if (value != null)
-                return (
-                    value.compagnie_name.toLowerCase().includes(searchWord?.toLowerCase()) ||
-                    value.site.toLowerCase().includes(searchWord?.toLowerCase()) ||
-                    value.telephone.toLowerCase().includes(searchWord?.toLowerCase()) ||
-                    value.adresse.includes(searchWord?.toLowerCase()) ||
-                    value.total_place_coupon.toString().includes(searchWord?.toLowerCase()) ||
-                    value.total_Place.toString().includes(searchWord?.toLowerCase())
-                );
-            else {
-                return ("")
-            }
-        });
-        return list;
-    };
-
-    const filterList: Info[] | [] = searchWord
-        ? globalSearch()
-        : info;
-
+    const allowedPageSizes = [5, 10, 20];
 
     return (
         <>
-            <div className="overflow-x-auto relative p-10 sm:rounded-lg lg:rounded-lg ">
+            <div className='p-3'>
+                <DataGrid id="gridContainer" dataSource={info} keyExpr="id"
+                    allowColumnReordering={true}
+                    allowColumnResizing={true}
+                    columnAutoWidth={true}
+                    showBorders={true}>
+                    <ColumnChooser enabled={true} />
+                    <HeaderFilter visible={true} />
+                    <FilterRow visible={true} />
+                    <GroupPanel visible={true} />
+                    <StateStoring enabled={true} type="localStorage" storageKey="storage" />
+                    <Pager showPageSizeSelector={true} allowedPageSizes={allowedPageSizes} />
+                    <SearchPanel visible={true} placeholder={"Search..."} />
+                    <FilterPanel visible={true} />
 
-                {/* Search Bar */}
-                <div className="pb-4 bg-white dark:bg-gray-900">
-                    <label htmlFor="table-search" className="sr-only">Search</label>
-                    <div className="relative mt-1">
-                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <input type="text" id="table-search"
-                            onChange={(event) => setSearchWord(event.target.value)}
-                            value={searchWord}
-                            className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Recherche de garderie" />
-                    </div>
-                </div>
 
-                {/* table */}
-                <table className="table-auto overflow-scroll w-full text-sm text-left text-gray-500 dark:text-gray-400">
 
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="py-3 px-6">
-                                nom de la compagnie
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Telephone
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Adresse
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Email
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Total de place coupon
-                            </th>
-                            <th scope="col" className="py-3 px-6">
-                                Total de place
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {filterList.map((i: Info) => (
-
-                            <tr key={i.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-slate-50">
-                                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-wrap dark:text-white">
-                                    {i.compagnie_name}
-                                </th>
-                                <td className="py-4 px-6 ">
-                                    <a href={"tel:" + i.telephone} className="hover:text-blue-600">{i.telephone}</a>
-                                </td>
-                                <td className="py-4 px-6">
-                                    {i.adresse}
-                                </td>
-                                <td className="py-4 px-6">
-                                    {i.site}
-                                </td>
-                                <td className="py-4 px-6">
-                                    {i.total_place_coupon}
-                                </td>
-                                <td className="py-4 px-6">
-                                    {i.total_Place}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                    <Column dataField="compagnie_name" caption={"Nom"}></Column>
+                    <Column dataField="telephone"></Column>
+                    <Column dataField="adresse" caption={"Address"}></Column>
+                    <Column dataField="site" caption={"Email"}></Column>
+                    <Column dataField="type" caption={"Type"}></Column>
+                    <Column dataField="total_place_coupon" />
+                    <Column dataField="total_Place"></Column>
+                </DataGrid>
             </div>
         </>
     )
